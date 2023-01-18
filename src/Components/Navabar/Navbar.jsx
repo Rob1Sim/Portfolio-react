c3eda2a3b656f2190396d1dd87f6631b4837c618
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2023-01-19 00:44:12 +0100

    Ajout de menu burger

--- src/Components/Navabar/Navbar.jsx
+++ src/Components/Navabar/Navbar.jsx
@@ -1,13 +1,21 @@
 import logo from "/public/assets/images/Logologo.png"
 import "./Navbar.scss"
+import { useState } from "react";
 
 //TODO: Ajouter un menu qui s'ouvre lorsque l'écran est trop petit
 function Navbar(){
 
+
+    const [showLinks, setShowLinks] = useState(false)
+
+    const handleShowLinks = ()=>{
+        setShowLinks(!showLinks)
+    }
+
     return(
         <nav className="Navbar">
             <a href="#"><img src={logo} /></a>
-            <ul className="display-big-screen nav-ul">
+            <ul className={`nav-ul ${showLinks ? "show-nav" : "dont-show"} `}>
                 <li><a href="#" className="nav-link">À propos</a></li>
                 <li><a href="#" className="nav-link">Mon parcours</a></li>
                 <li><a href="#" className="nav-link">Mes projets</a></li>
@@ -15,7 +23,7 @@ function Navbar(){
                 <li><a href="#" className="nav-cv">CV</a></li>
 
             </ul>
-            <button className="nav_burger">
+            <button className="nav_burger" onClick={handleShowLinks}>
                 <span className="burger_bar"></span>
             </button>
         </nav>
