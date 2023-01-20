5bc0a77e79ce00752cfb6cddbb7812702a44dda0
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2023-01-20 20:15:25 +0100

    Correction de fautes d'orthographes

--- src/Components/Content/Description/Description.jsx
+++ src/Components/Content/Description/Description.jsx
@@ -7,17 +7,17 @@ function Description(){
         <div className="description-row">
             <section className="description" id='about'>
                 <h2>À propos</h2>
-                <p>Je m'appelle Robin Simonneau, j'ai commencé à programmer assez jeune à l'époque pour créer des mods pour Minecraft et depuis je ne me suis jamais arrêter.
-                    J'ai pue essayer plein de languages de programmation et de technologies différentes comme : Java, Python, Javascript, et les fondamentaux du web !
+                <p>Je m'appelle Robin Simonneau, j'ai commencé à programmer assez jeune à l'époque pour créer des mods pour Minecraft et depuis je ne me suis jamais arrêté.
+                    J'ai pu essayer plein de languages de programmation et de technologies différentes comme : Java, Python, Javascript, et les fondamentaux du web !
                 </p>
                 <p>
-                    Aujourd'hui j'étudie le développement à web à l'Institut Universitaire de Technologie (IUT) de Reims, on y apprend des technologies comme PHP et Symfony, C# et .NET et pleins d'autre.
-                    J'y apprend aussi les bases de la comptabilité, du droit de l'informatique, et de la gestion de projet !
+                    Aujourd'hui j'étudie le développement à web à l'Institut Universitaire de Technologie (IUT) de Reims, on y apprend des technologies comme PHP et Symfony, C# et .NET et pleins d'autres.
+                    J'y apprends aussi les bases de la comptabilité, du droit de l'informatique, et de la gestion de projet !
                 </p>
                 <p>
-                    Evidemment en grand curieux que je suis, je suis toujours à la recherche de nouvelles choses à apprendre, à l'heure ou j'écris ces lignes c'est <strong>React</strong>, ce site est d'ailleurs réalisé avec React .
+                Évidemment en grand curieux que je suis, je suis toujours à la recherche de nouvelles choses à apprendre, à l'heure ou j'écris ces lignes c'est <strong>React</strong>, ce site est d'ailleurs réalisé avec React .
                 </p>
-                <p>Voici une petite listes des technologies que j'utilise</p>
+                <p>Voici une petite liste des technologies que j'utilise :</p>
                 <div className="skills">
                     <ul>
                         <li> PHP</li>
