5866e43a286a1501b69eb27dc426b901758883d8
Robin Simonneau <simonneaur@cy-tech.fr>
2023-10-03 10:26:27 +0200

    Correction orthographe

--- src/Components/Content/About/About.jsx
+++ src/Components/Content/About/About.jsx
@@ -6,7 +6,7 @@ function About(){
         <article className="about">
             <h1>Robin Simonneau</h1>
             <h2>Ingénieur en devenir</h2>
-            <p>Actuellement étudiant en informatique à CY Tech, en formation de d'ingénieur informatique en alternance.</p>
+            <p>Actuellement étudiant en informatique à CY Tech, en formation d'ingénieur informatique en alternance.</p>
             <a href="#projects">Découvrir ce que je fais</a>
         </article>
     )
